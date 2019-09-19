7c08b98621a7a454fb2f43ea633c289646300e31
IsaacMendoza-dv <ca.isaac.mendoza@gmail.com>
2019-09-18 20:28:36 -0500

    [MOD] excercise

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var exchangePenToMxn = 5.81
+var pen = 2500
+
+var totalMxn = pen * exchangePenToMxn
+
+var exchangeMxnToUsd = 0.052
+var totalUSD = totalMxn * exchangeMxnToUsd
+
+var exchangeMxnToEur = 0.047
+var totalEur = totalMxn * exchangeMxnToEur
+
+console.log('Tengo ' + totalUSD + ' Dolares')
+
+console.log('Tengo ' + totalEur + ' Euros')
