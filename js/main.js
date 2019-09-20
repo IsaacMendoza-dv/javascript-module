15283a658118ac57a84ebcd912feac2cf0250cf8
IsaacMendoza-dv <ca.isaac.mendoza@gmail.com>
2019-09-20 11:44:03 -0500

    [MOD] index.html

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
-var pets = ['dog', 'cat', 'mouse']
+var volcals = ['a','e','i','o','u']
+var word = "padrino";
 
-for( sizeArray = 0; sizeArray < pets.length; sizeArray++){
-    console.log(pets[sizeArray])
-}
+for (countV = 0 ;  )
 
- 
+
+
+
+
