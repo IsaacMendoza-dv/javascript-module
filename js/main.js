07f319fe00061f46b1a84f1c3b212bf606ca8e17
IsaacMendoza-dv <ca.isaac.mendoza@gmail.com>
2019-10-03 23:55:39 -0500

    :octocat:

--- js/main.js
+++ js/main.js
@@ -355,12 +355,32 @@ var matrixKoders = [
 var xhttp = new XMLHttpRequest()
 xhttp.onreadystatechange = function () {
   if (this.readyState === 4 && this.status === 200) {
-    let objRes = JSON.parse(this.response)
-    handlerResponse(objRes)
+    let response = JSON.parse(this.response)
+    // handlerResponse(response)
   }
 }
-xhttp.open('GET', "data/hello_world.json")
-xhttp.send()
-const handlerResponse = (obj) => {
-  console.log(obj)
+var koder = {
+  firstName: 'Cristina',
+  lastName: 'Bermudez',
+  generation: 5,
+  favoritePerson: 'me :)',
+  imaginationForThisExcercise:'none',
+  favoriteColor: 'black',
+  favoriteNumber: 5,
+  adress : {
+    street:'Corregidora',
+    houseNumber : '40',
+    city : 'CDMX',
+  }
 }
+xhttp.open('POST', "https://ajaxkode.firebaseio.com/.json")
+xhttp.send(JSON.stringify(koder))
+// const handlerResponse = (response) => {
+//   console.log(response)
+//   var parent = document.getElementById('container')
+//   response.forEach(({ element, text }) => {
+//     let node = document.createElement(element)
+//     node.innerText = text
+//     parent.appendChild(node)
+//   });
+// }
